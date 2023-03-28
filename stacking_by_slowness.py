c3efe6b4c555dd349db1c0222a47606f76039619
Madeleine Tan <97422443+madeleine-tan@users.noreply.github.com>
2023-03-27 20:51:22 -0400

    Update stacking_by_slowness.py

--- stacking_by_slowness.py
+++ stacking_by_slowness.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.use('macosx')
 
+from obspy.taup import TauPyModel
+model = TauPyModel(model="ak135")
+
 def _fig1(name, data_all, data45, data56, data67, data78):
     """
     Plot 5 stack traces on figure by station: all slowness values in s/km, [0.04-0.05), [0.05-0.06), [0.06-0.07), [0.07-0.08]
